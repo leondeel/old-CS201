30ea57204e1f4c409abbe1aca2d2000ee657befc
Ariel Hernandez <leondeel@gmail.com>
2020-02-02 23:44:03 -0900

    It is done.

--- homework1/Source.cpp
+++ homework1/Source.cpp
@@ -6,26 +6,46 @@ using std::endl;
 
 int main(int argc, char** argv) {
 
-	int UserInput, SpacesNeededinRow, ColumnNumber = 1, RowNumber = 1, HashtagsNeededInRow = 1;
+	int UserInput, SpacesNeededinRow, HashtagsInRow = 1, HashtagsNeededInRow = 1, RowNumber = 1;
 	cout << "Enter a positive integer:  ";
 	cin >> UserInput;
 	while (RowNumber <= UserInput) {
 		SpacesNeededinRow = UserInput - RowNumber;
-		while (ColumnNumber <= HashtagsNeededInRow) {
+		while (HashtagsInRow <= HashtagsNeededInRow) {
 			while (SpacesNeededinRow>0) {
 				cout << " ";
 				SpacesNeededinRow--;
 			}
 			cout << "#";
-			ColumnNumber++;
+			HashtagsInRow++;
 		}
 		cout << endl;
-		ColumnNumber = 1;
+		HashtagsInRow = 1;
 		HashtagsNeededInRow = 2*RowNumber+1;
 		RowNumber++;
 		
 	}
 
+	RowNumber = UserInput-1;  //Everything is going to be backwards now.  We will be counting the RowNumber down to zero instead of up from 1 as before. The second half of this code is similar but different to the first half.
+	HashtagsNeededInRow = 2*(RowNumber-1) +1;
+
+	while (RowNumber >0) {
+		SpacesNeededinRow = UserInput-RowNumber;
+		while (HashtagsInRow <= HashtagsNeededInRow) {
+			while (SpacesNeededinRow > 0) {
+				cout << " ";
+				SpacesNeededinRow--;
+			}
+			cout << "#";
+			HashtagsInRow++;
+		}
+		cout << endl;
+		HashtagsInRow = 1;
+		RowNumber--;
+		HashtagsNeededInRow = 2 * (RowNumber - 1) + 1;
+		
+
+	}
 
 
 
